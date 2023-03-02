d831856fca4acc150845a59691b547c7ed7883e1
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2023-03-02 15:00:53 +0700

    Forgot API
    send email

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,7 +3,10 @@ const AuthSchema = require("../models/auth");
 const ApiError = require("../utils/ApiError");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+
 const SERECT_KEY = process.env.SERECT_KEY;
+
 exports.register = catchAsync(async (req, res) => {
   const { name, email, password, age } = req.body;
   const existedAuth = await AuthSchema.create({
@@ -20,11 +23,11 @@ exports.login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const existedEmail = await AuthSchema.findOne({ email });
   if (!existedEmail) {
-    throw new ApiError(400, "Unauthorize");
+    throw new ApiError(400, "email or password is not valid");
   }
   const isMatch = bcrypt.compareSync(password, existedEmail.password);
   if (!isMatch) {
-    throw new ApiError(300, "Unauthorize");
+    throw new ApiError(400, "email or password is not valid");
   }
   const token = jwt.sign(
     {
@@ -35,4 +38,32 @@ exports.login = catchAsync(async (req, res) => {
     { expiresIn: "1d" }
   );
   res.status(200).json({ succes: true, token: token });
+});
+
+exports.forget = catchAsync(async (req, res) => {
+  const { email } = req.body;
+  const existedEmail = await AuthSchema.findOne({ email });
+  if (!existedEmail) {
+    throw new ApiError(400, "email or password is not valid");
+  }
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.PASSWORD, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "ngocbao123steam@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+  res.status(201).json({ sucess: true });
 });
