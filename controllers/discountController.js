3231171f0629ad72ebf43b169a39171914e473c0
NguyenNgocBao <ititiu19084@student.hcmiu.edu.vn>
2023-04-30 20:32:25 +0700

    Update discountController.js

--- controllers/discountController.js
+++ controllers/discountController.js
@@ -1,6 +1,14 @@
 const catchAsync = require("../middlewares/catchAsync");
+const discountSchema = require("../models/discount");
 const ApiError = require("../utils/ApiError");
 
 exports.createDiscount = catchAsync(async (req, res) => {
   const { name, description, discountPersent, isActive } = req.body;
+  const discount = await discountSchema.create({
+    name,
+    description,
+    discountPersent,
+    isActive,
+  });
+  res.status(201).json({ success: true, data: discount });
 });
